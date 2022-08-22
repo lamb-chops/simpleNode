cf09aa621c2ad13f7d1d1a75cdec106850e22d23
David Lambert <david.lambert@my.utsa.edu>
2022-08-22 08:44:48 -0500

    filenames return in same order from promises

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         console.log(err)
     }
-    console.log(filenames)
+    const statPromises = filenames.map(filename => {
+            return fs.lstat(filename)
+        })
+        //array of promises, waits for all to finish
+    const allStats = await Promise.all(statPromises)
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats)
+        console.log(filenames[index], stats.isFile())
+    }
 })
